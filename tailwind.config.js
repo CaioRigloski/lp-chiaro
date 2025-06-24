17cf4000a28063791d8616079ab6a79c27355772
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-06-24 02:34:02 -0300

    feat: texts until third page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        seasons_light: ['TheSeasonsLight', 'serif']
+      }
+    },
+  },
+  plugins: [],
+};
